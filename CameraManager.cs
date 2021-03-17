7016e1286ed70fa65a32763da42a095b51fc8314
Cristian Susanu <cristian.augustin.susanu@gmail.com>
2021-03-17 21:51:11 +0200

    Updated camera views

--- CameraManager.cs
+++ CameraManager.cs
@@ -7,37 +7,52 @@ public class CameraManager : MonoBehaviour
     public GameObject car;
 
     //third person view
-    public float cameraDistance = 5f;
-    public float cameraHeight = 2f;
-    public float dampening = 1f;
+    public float cameraDistance = 4f;
+    public float cameraHeight = 1.7f;
+    public float dampening = 5f;
 
     //first person view
     public float driverViewHeight = 1.01f;
     public float distance2 = -0.28f;
-    public float lelngth2 = -0.33f;
+    public float length2 = -0.33f;
+
+    //bonet view
+    public float bonetViewHeight = 1.3f;
+    public float bonetHeight = 0.4f;
 
     private int cameraMode = 0;
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.C))
         {
-            cameraMode = (cameraMode + 1) % 2; //where 2 is the number of cameras in use
+            cameraMode = (cameraMode + 1) % 3; //where 2 is the number of cameras in use
         }
 
         switch (cameraMode)
         {
+            //fps view
             case 1:
-                transform.position = car.transform.position + car.transform.TransformDirection(new Vector3(lelngth2, driverViewHeight, distance2));
+                //transform.position = car.transform.position + car.transform.TransformDirection(new Vector3(lelngth2, driverViewHeight, distance2));
+                transform.position = Vector3.Lerp(transform.position, car.transform.position + car.transform.TransformDirection(new Vector3(distance2, driverViewHeight, length2)), dampening * Time.deltaTime);
                 transform.rotation = car.transform.rotation;
                 Camera.main.fieldOfView = 55f;
                 break;
+            //bonet view
+            case 2:
+                //transform.position = car.transform.position + car.transform.TransformDirection(new Vector3(0, bonetViewHeight, bonetHeight));
+                transform.position = Vector3.Lerp(transform.position, car.transform.position + car.transform.TransformDirection(new Vector3(0f, bonetViewHeight, bonetHeight)), dampening * Time.deltaTime);
+                transform.rotation = car.transform.rotation;
+                Camera.main.fieldOfView = 65f;
+                break;
+            //behind the car view
             default:
                 //distance is negative because we are behind the car on the z axis
+                //transform.position = car.transform.position + car.transform.TransformDirection(new Vector3(0f, cameraHeight, -cameraDistance));
                 transform.position = Vector3.Lerp(transform.position, car.transform.position + car.transform.TransformDirection(new Vector3(0f, cameraHeight, -cameraDistance)), dampening * Time.deltaTime);
                 transform.LookAt(car.transform);
-                //Camera.main.fieldOfView = 80f;
                 break;
         }
     }
