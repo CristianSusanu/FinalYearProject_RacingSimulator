eb8770b01c810a14b254381650b4082d204adc10
Cristian Susanu <cristian.augustin.susanu@gmail.com>
2021-04-11 18:10:07 +0300

    Added Car Selector and Main Menu

---
+++ CarSelectorManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSelectorManager : MonoBehaviour
+{
+    public CarSelectorList carList;
+    //public GameObject car;
+    public GameObject carStand;
+    public float standRotationSpeed = 0f;
+    public int vehiclePointer = 0;
+
+    private void Awake()
+    {
+        vehiclePointer = PlayerPrefs.GetInt("pointer");//get the pointer
+
+        //instantiate the vehicle stored in the pointer
+        GameObject individualCar = Instantiate(carList.cars[vehiclePointer], Vector3.zero, Quaternion.identity) as GameObject;
+        individualCar.transform.parent = carStand.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        /*
+        car = GameObject.FindGameObjectWithTag("Car");
+        car.transform.Rotate(Vector3.up * standRotationSpeed * Time.deltaTime);
+        carStand.transform.Rotate(Vector3.up * standRotationSpeed * Time.deltaTime);
+        */
+
+        carStand.transform.Rotate(Vector3.up * standRotationSpeed * Time.deltaTime);
+    }
+
+    public void NextButton()
+    {
+        if (vehiclePointer < carList.cars.Length - 1)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("Car"));
+            vehiclePointer++;
+            PlayerPrefs.SetInt("pointer", vehiclePointer);
+            GameObject individualCar = Instantiate(carList.cars[vehiclePointer], Vector3.zero, Quaternion.identity) as GameObject;
+            individualCar.transform.parent = carStand.transform;
+        }
+    }
+
+    public void PreviousButton()
+    {
+        if (vehiclePointer > 0)
+        {
+            Destroy(GameObject.FindGameObjectWithTag("Car"));
+            vehiclePointer--;
+            PlayerPrefs.SetInt("pointer", vehiclePointer);
+            GameObject individualCar = Instantiate(carList.cars[vehiclePointer], Vector3.zero, Quaternion.identity) as GameObject;
+            individualCar.transform.parent = carStand.transform;
+        }
+    }
+}
---
+++ MenuLoadBar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; //to reference slider
+
+public class MenuLoadBar : MonoBehaviour
+{
+    public GameObject loadScreen;
+    public Slider loadSlider;
+    public Text loadText;
+
+    public void LevelLoading(int sceneNumber)
+    {
+        StartCoroutine(LoadSceneAsyncronously(sceneNumber));
+
+        //to accomodate for when the time is 0 for winning screens
+        if (Time.timeScale == 0f)
+        {
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
+    }
+
+    IEnumerator LoadSceneAsyncronously(int sceneNumber)
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(sceneNumber);//keeps the current scene running while it's loading the new scene into memory
+        loadScreen.SetActive(true);
+
+        while (!op.isDone)
+        {
+            float progress = Mathf.Clamp01(op.progress / 0.9f);
+            loadSlider.value = progress;
+            loadText.text = progress * 100f + "%";
+            
+            yield return null;//to wait until the next frame before continuing
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        //Debug.Log("Game Closed");
+    }
+}
